7f08dddfed620fd91e338965494b6c02f6ae3543
Gregor Goldbach <goldbach@dfn-cert.de>
2009-01-22 19:45:39 +0000

    Added some translations

--- lib/Perl/Critic/I18N.pm
+++ lib/Perl/Critic/I18N.pm
@@ -1,8 +1,8 @@
 ##############################################################################
-#      $URL: $
-#     $Date: $
-#   $Author: $
-# $Revision: $
+#      $URL$
+#     $Date$
+#   $Author$
+# $Revision$
 ##############################################################################
 package Perl::Critic::I18N;
 
@@ -10,24 +10,29 @@ use Locale::Maketext;
 use base qw(Locale::Maketext Exporter);
 
 # We export one function by default. This is our translator.
-our @EXPORT=qw(__);
+our @EXPORT = qw(translate);
 
-# Get 'handle' for the language we ought to translate to.
-our $language_handle = Perl::Critic::I18N->get_handle() || die "Unable to get language handle";
+{
+	# Get 'handle' for the language we ought to translate to.
+	my $language_handle = Perl::Critic::I18N->get_handle()
+	  || die 'Unable to get language handle';
 
-# Our translation function just calls maketext() for the specific language.
-sub __ { $language_handle->maketext(@_) };
+	# Our translation function just calls maketext() for the specific language.
+	sub translate {
 
-# Taken from Locale::Maketexts man page: If maketext fails, we want to see who called it.
-$Carp::Verbose = 1;
+		# Taken from Locale::Maketexts man page: If maketext fails, we want to see who called it.
+		local $Carp::Verbose = 1;
+		$language_handle->maketext(@_);
+	}
+}
 
 # I decree that this project's first language is English.
 
 %Lexicon = (
 	'_AUTO' => 1,
 
-    # The following lines are taken from the sample module File::Findgrep.
-    
+	# The following lines are taken from the sample module File::Findgrep.
+
 	# That means that lookup failures can't happen -- if we get as far
 	#  as looking for something in this lexicon, and we don't find it,
 	#  then automagically set $Lexicon{$key} = $key, before possibly
--- lib/Perl/Critic/I18N/de.pm
+++ lib/Perl/Critic/I18N/de.pm
@@ -1,8 +1,8 @@
 ##############################################################################
-#      $URL: $
-#     $Date: $
-#   $Author: $
-# $Revision: $
+#      $URL$
+#     $Date$
+#   $Author$
+# $Revision$
 ##############################################################################
 package Perl::Critic::I18N::de;
 
@@ -18,10 +18,25 @@ our %Lexicon = (
 'Warnung: Die Optionen --profile und --noprofile können nicht zusammen verwendet werden.',
 
 	'Warning: --verbose arg "[_1]" looks odd.  '
-	  . 'Perhaps you meant to say "--verbose 3 [_1]."'
-	  => 'Warnung: Das Argument "[_1]" für --verbose sieht merkwürdig aus.  '
-	  . 'Vielleicht meinten Sie "--verbose 3 [_1]."'
+	  . 'Perhaps you meant to say "--verbose 3 [_1]."' =>
+	  'Warnung: Das Argument "[_1]" für --verbose sieht merkwürdig aus.  '
+	  . 'Vielleicht meinten Sie "--verbose 3 [_1]."',
 
+	'Warning: --top argument "[_1]" is negative.  '
+	  . 'Perhaps you meant to say "[_1] --top".' =>
+	  'Warnung: Das Argument "[_1]" für --top ist negativ.  '
+	  . 'Vielleicht meinten Sie "[_1] --top".',
+
+	'Warning: --severity arg "[_1]" out of range.  '
+	  . 'Severities range from "[_2]" (lowest) to '
+	  . '"[_3]" (highest).' =>
+'Warnung: Das Argument "[_1]" für --severity liegt außerhalb des gültigen Bereichs.  '
+	  . 'Die Werte reichen von "[_2]" (niedrigster Wert) bis '
+	  . '"[_3]" (höchster Wert).',
+	  
+	  'Nothing to critique.' => 'Es gibt nichts zu kritisieren.',
+	  'No such file or directory: "[_1]"' => 'Es gibt weder eine Datei noch ein Verzeichnis dieses Namens: "[_1]"',
+'No perl files were found'=>'Es wurden keine Perl-Dateien gefunden'
 );
 
-1;                                                        
+1;
